f09f8d20b1d89e4c3ce09fa350894dbcc3c2fb9e
Rajveer Singh Chauhan <rajveersinghchauhan2003@gmail.com>
2024-03-03 11:20:10 +0530

    Nums and Maths

---
+++ 01_Basics/09_Nums_Maths.js
@@ -0,0 +1,34 @@
+// const num1 = 400
+// const num2 = new Number(10000)
+
+// console.log(num1);
+// console.log(typeof num1);
+// console.log(num2);
+// console.log(typeof num2);
+// console.log(typeof num2.toString());
+// console.log(num2.toFixed(2));
+// console.log(num2.toLocaleString('en-IN'));  // comma vegera laga deta hai paramater ka matlab Indian standard as default parameter me American standard rehta hai.
+// const num3 = new Number(123.8998)
+// console.log(num3.toPrecision(2));  // iska khel tagda hai samajhna.
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MAX_SAFE_INTEGER);
+
+
+// // ----------------------- Maths -------------------------------------------------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+// // round , ceil , floor , min  max 
+
+console.log(Math.random()); // between 0 and 1
+
+// for a range :
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
